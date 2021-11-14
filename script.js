079af11ab8022161e75204b4ee352b49566845e9
Rohit Raj <rohitrajvns2020@gmail.com>
2021-11-14 21:18:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var x = 6*Math.random();
+var y = 6*Math.random();
+x = Math.floor(x);
+y = Math.floor(y);
+x = x+1;
+y = y+1;
+
+if (x==1) {
+    document.querySelector("#player1").setAttribute("src", "images/1.png");
+}
+else if(x==2) {
+    document.querySelector("#player1").setAttribute("src", "images/2.png");
+}
+else if(x==3) {
+    document.querySelector("#player1").setAttribute("src", "images/3.png");
+}
+else if(x==4) {
+    document.querySelector("#player1").setAttribute("src", "images/4.png");
+}
+else if(x==5) {
+    document.querySelector("#player1").setAttribute("src", "images/5.png");
+}
+else if(x==6) {
+    document.querySelector("#player1").setAttribute("src", "images/6.png");
+}
+
+if (y==1) {
+    document.querySelector("#player2").setAttribute("src", "images/1.png");
+}
+else if(y==2) {
+    document.querySelector("#player2").setAttribute("src", "images/2.png");
+}
+else if(y==3) {
+    document.querySelector("#player2").setAttribute("src", "images/3.png");
+}
+else if(y==4) {
+    document.querySelector("#player2").setAttribute("src", "images/4.png");
+}
+else if(y==5) {
+    document.querySelector("#player2").setAttribute("src", "images/5.png");
+}
+else if(y==6) {
+    document.querySelector("#player2").setAttribute("src", "images/6.png");
+}
+
+if(x>y){
+    document.querySelector(".result").innerHTML = "Player 1 Wins"
+}
+else if(x<y){
+    document.querySelector(".result").innerHTML = "Player 2 Wins"
+}
+else{
+    document.querySelector(".result").innerHTML = "It's a Tie"
+}
